24baabefd3177967dc19fc69c6cb5b7593632af2
Andrew <afonse@udel.edu>
2023-09-08 17:17:45 -0400

    Added a header, image with alt text, list with three elements, background color of header, bootstrap button with logging, two-column layout, and red rectangles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Button, Container, Col, Row } from "react-bootstrap";
 import "./App.css";
+import image from "./huh.webp";
 
 function App(): JSX.Element {
     return (
@@ -7,6 +9,43 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1 className="App-header2">
+                {" "}
+                Added another header and changed its background color{" "}
+            </h1>
+            <ul className="uls">
+                <li> Mango </li>
+                <li> Apple </li>
+                <li> Orange </li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <img src={image} alt="4k machine broke" />
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "40",
+                                height: "400px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                        1st Column
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "40",
+                                height: "400px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                        2nd Column
+                    </Col>
+                </Row>
+            </Container>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World - Andrew Fonseca
